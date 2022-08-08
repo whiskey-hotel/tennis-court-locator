71e86425f2e5d23c05ad29ed24a69639dbf0755a
Justin Thornton <jathom15@gmail.com>
2022-08-08 08:18:44 -0500

    Python script for google API calls

--- grabPNG.py
+++ grabPNG.py
@@ -1,1 +1,29 @@
-print('Hello World')
+import requests 
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+jsonFile = open('tennis_courts.json')
+data = json.load(jsonFile)
+
+api_key = os.environ.get("Google_API_KEY")
+
+url = "https://maps.googleapis.com/maps/api/staticmap?"
+zoom = '20'
+maptype = 'satellite'
+size = '640x640'
+
+for i in range(6): 
+    lat = data[i]['latitude']
+    long = data[i]['longitude']
+    name = data[i]['name']
+
+    r = requests.get(f"{url}center={lat},{long}&zoom={zoom}&size={size}&maptype={maptype}&key={api_key}") 
+    f = open(f'tennis-court-locator/dataset/images/{name}.png', 'wb') 
+    
+    f.write(r.content) 
+    f.close() 
+
+jsonFile.close()
+
